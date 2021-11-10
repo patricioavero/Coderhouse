913e3c0e85fed1559ff72c1acf1f5b3e9530b2fa
Patricio Avero <patricio.avero@gmail.com>
2021-11-10 19:42:26 -0300

    in-class-03 files

---
+++ in-class-03-02.py
@@ -0,0 +1,16 @@
+## Class 03
+## In-class Excercise
+
+## Given tuple
+cities = (
+    "San Jorge", "Quitilipi", "Granadero Baigorria", "Alderetes", "Gualeguay", "Chimbas", "Rada Tilly", "Cote-Lai", "Elena", "Yerba Buena", "Laboulaye", "La Rioja", "Castelli", "Aminga", "Isla Verde", "Pirane", "General Enrique Godoy", "Alumine", "Rio Pico", "Sampacho", "Villa de Soto", "Embarcacion", "Caseros", "Jesus Maria", "Salta",
+    "Esquina",
+    "Puerto Iguazu", "Mercedes", "Castelli", "Cinco Saltos", "El Bolson", "Tamberias", "Guernica", "Doblas", "Villa Santa Rita", "Mercedes", "Carlos Casares", "Vera", "Zapala", "Santa Ana", "Libertador General San Martin", "Villa Maria", "Basail", "Gualeguay", "Villa La Angostura", "Basail", "San Lorenzo", "Tostado", "Libertad", "General Conesa", "General Pico"
+    )
+## Some calculations to variables
+tuple_total = len(cities)
+tuple_middle = tuple_total / 2
+
+
+print(tuple_total)
+print(tuple_middle)
