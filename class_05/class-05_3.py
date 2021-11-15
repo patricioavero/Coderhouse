0754b54ba4823bf016dd46a318cc20e549eb87ce
Patricio Avero <patricio.avero@gmail.com>
2021-11-15 19:30:19 -0300

    class-05_3.py

---
+++ class_05/class-05_3.py
@@ -0,0 +1,14 @@
+##---------------------------------------------------------------------
+## CODERHOUSE
+##  Student     : AVERO, Patricio
+##  Course      : Python
+##  Commission  : 23855
+##  Tutor       : ZUAZO, Joaquín
+##  Class       : 05
+##  Date        : 08-NOV-2021 & 10-NOV_2021
+##  Challenge   : https://docs.google.com/document/d/1_U-mHyF6nTwHJrHsgWt9oUQ-WIDz_mdcKIC97r6W3o4/edit
+##  Notes       : 
+##---------------------------------------------------------------------
+
+## Add and print al the odd numbers from the range collection
+print(sum(range(1, 100, 2)))
