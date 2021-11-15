12b70e3959e955d39d7b0d4693af4a6474aa63b9
Patricio Avero <patricio.avero@gmail.com>
2021-11-15 20:07:41 -0300

    class-05_4.py

---
+++ class_05/class-05_4.py
@@ -0,0 +1,23 @@
+##---------------------------------------------------------------------
+## CODERHOUSE
+##  Student     : AVERO, Patricio
+##  Course      : Python
+##  Commission  : 23855
+##  Tutor       : ZUAZO, Joaquín
+##  Class       : 05
+##  Date        : 08-NOV-2021 & 10-NOV_2021
+##  Challenge   : https://docs.google.com/document/d/1_U-mHyF6nTwHJrHsgWt9oUQ-WIDz_mdcKIC97r6W3o4/edit
+##  Notes       : 
+##---------------------------------------------------------------------
+
+## Inser the times of numbers to enter
+times = int(input("Enter how many number you want to average: "))
+
+## Loop all over the times entered
+res = float(0)
+for time in range(1, times+1, 1):
+    num = float(input(f"Enter the {time} number:"))
+    res = res + num
+
+## Average
+print("The average of the numbers is: ", res / times)
