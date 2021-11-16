858ac83b99472c1ba60532ca4a9f34a7d7007f4f
ext_paavero <ext_paavero@mercadolibre.com>
2021-11-16 11:18:16 -0300

    class-05_6.py

---
+++ class_05/class-05_6.py
@@ -0,0 +1,31 @@
+##---------------------------------------------------------------------
+## CODERHOUSE
+##  Student     : AVERO, Patricio
+##  Course      : Python
+##  Commission  : 23855
+##  Tutor       : ZUAZO, Joaquín
+##  Class       : 05
+##  Date        : 08-NOV-2021 & 10-NOV_2021
+##  Challenge   : https://docs.google.com/document/d/1_U-mHyF6nTwHJrHsgWt9oUQ-WIDz_mdcKIC97r6W3o4/edit
+##  Notes       : 
+##---------------------------------------------------------------------
+
+# Todos los números del 0 al 10 [0, 1, 2, ..., 10]
+list_of_nums = list(range(10))
+print("Todos los números del 0 al 10 [0, 1, 2, ..., 10]\t->\t", list_of_nums, "\n")
+
+# Todos los números del -10 al 0 [-10, -9, -8, ..., 0]
+list_of_nums = list(range(-10, 0))
+print("Todos los números del -10 al 0 [-10, -9, -8, ..., 0]\t->\t", list_of_nums, "\n")
+
+# Todos los números pares del 0 al 20 [0, 2, 4, ..., 20]
+list_of_nums = list(range(0, 21, 2))
+print("Todos los números pares del 0 al 20 [0, 2, 4, ..., 20]\t->\t", list_of_nums, "\n")
+
+# Todos los números impares entre -20 y 0 [-19, -17, -15, ..., -1]
+list_of_nums = list(range(-20, 0))
+print("Todos los números impares entre -20 y 0 [-19, -17, -15, ..., -1]\t->\t", list_of_nums, "\n")
+
+# Todos los números múltiples de 5 del 0 al 50 [0, 5, 10, ..., 50]
+list_of_nums = list(range(0, 51, 5))
+print("Todos los números múltiples de 5 del 0 al 50 [0, 5, 10, ..., 50]\t->\t", list_of_nums)
