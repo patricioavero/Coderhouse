b95020048ab1f699824d6101a44914c002330cb1
patricioavero <patricio.avero@gmail.com>
2021-11-28 13:40:17 -0300

    Fix some typos...

--- class_09/class-09.py
+++ class_09/class-09.py
@@ -9,12 +9,21 @@
 ##  Challenge   : https://docs.google.com/presentation/d/1PbUtfvt9M7oQzSm07rM759PAxwaDweR87g6GAqyNWuk/preview#slide=id.ged1c8e7111_1_60
 ##  Notes       : 
 ##---------------------------------------------------------------------
-# Lets do some imports
+
+##----------------------------------------
+## Imports
+##----------------------------------------
 import os
 
-# Set some variables
+##----------------------------------------
+## Variables
+##----------------------------------------
 option = str('y')
 
+##----------------------------------------
+## Functions
+##----------------------------------------
+# To clear the screen
 def clear_screen ():
     os_name = os.name
     if (os_name.lower() == 'posix'):
@@ -22,16 +31,18 @@ def clear_screen ():
     elif (os_name.lower() == 'nt'):
         os.system('cls')
 
+# To print the menu
 def menu ():
     print("*** Lepar-Year Checker ***")
     year = int(input("Enter the year you want to check ---> "))
     return year
 
+# To check if programs continues
 def quit ():
-    option = str(input("Do you want to test another yeear? [y/n]) "))
+    option = str(input("Do you want to check another year? [y/n]) "))
     return option
 
-# Function to evaluate divisor number
+# To evaluate divisor number
 def divisor (dividend, divisor):
     quotient = dividend % divisor
     if (quotient == 0):
@@ -39,6 +50,7 @@ def divisor (dividend, divisor):
     else:
         return 0
 
+# To check if the year is leap-year or not
 def leap_or_not (year):
     if divisor(year, 4):
         leap_condition = 1
@@ -54,12 +66,16 @@ def leap_or_not (year):
     if leap_condition == 0:
         return (0, year)
 
+# To print the message on STDOUT 
 def print_leap (leap_bool):
     if leap_bool[0] == 1:
         print(f"The year {leap_bool[1]} IS a leap-year")
     else:
         print(f"The year {leap_bool[1]} IS NOT a leap-year")
-
+        
+##----------------------------------------
+## Main
+##----------------------------------------
 while option.lower() == 'y':
     clear_screen()
     print_leap(leap_or_not(menu()))
